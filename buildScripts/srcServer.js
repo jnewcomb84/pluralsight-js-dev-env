941fc7dffd41f3ec6545306b3c44caf2b5d62276
newcomj <jeremy.newcomb@igt.com>
2018-02-01 14:59:58 -0800

    Adding .nsprc file to test Node Security for proxy setup. Having problems building the project on the corporate network.

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,7 +4,7 @@ import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
-const port = 3000;
+const port = 8000;
 const app = express();
 const compiler = webpack(config);
 
