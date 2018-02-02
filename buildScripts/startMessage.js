9703ed8dbb9875ae79ede8f9f757649ca8e5ed09
Jeremy Newcomb <Jeremy.Newcomb@IGT.com>
2018-02-02 15:17:19 -0800

    Comiitting to test out connection over corp network to github repo

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,4 @@
 import chalk from 'chalk';
+import {port} from './srcServer';
 
-console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
+console.log(chalk.green('Starting app on port ' + port + ', in dev mode...')); // eslint-disable-line no-console
