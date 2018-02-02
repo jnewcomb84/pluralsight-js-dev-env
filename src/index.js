837ca1fe7f87b926486f1cbb5ff7ecbb11bad86f
Jeremy Newcomb <Jeremy.Newcomb@IGT.com>
2018-02-02 09:12:33 -0800

    Uncommented the proxy settings for .nsprc since I'm on corp network and changed the API caller to demonstrate 'function' as being the same as =>

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import './index.css';
 import {getUsers} from './api/userApi';
 
 // Populate table of users via API call
-getUsers().then(result =>{
+
+getUsers().then(function(result){
     let usersBody = "";
 
     result.forEach(user =>{
@@ -16,3 +17,18 @@ getUsers().then(result =>{
     });
     global.document.getElementById('users').innerHTML = usersBody;
 });
+
+/*getUsers().then(result =>{
+    let usersBody = "";
+
+    result.forEach(user =>{
+        usersBody += `<tr>
+            <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+            <td>${user.id}</td>
+            <td>${user.firstName}</td>
+            <td>${user.lastName}</td>
+            <td>${user.email}</td>
+            </tr>`
+    });
+    global.document.getElementById('users').innerHTML = usersBody;
+});*/
